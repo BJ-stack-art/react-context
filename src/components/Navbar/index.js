9a1433a5b1162a6fabad5ed67ccc3c5019983c02
BJ-stack-art <58924072+BJ-stack-art@users.noreply.github.com>
2020-10-24 22:15:21 +0700

    08 - Consuming Multiple Context

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,8 @@
 import React , {Component} from 'react';
 
 import {ThemeContext} from '../../contexts/ThemeContext.js';
+import {AuthContext} from '../../contexts/AuthContext.js';
+
 
 const Style = {
 	menu: {
@@ -15,21 +17,28 @@ const Style = {
 class Navbar extends Component {
 	render() {
 		return(
-			<ThemeContext.Consumer>{(context) => {
-				const {isLightTheme , light , dark}  = context;
-				const theme = isLightTheme ? light : dark;
-				return(
-					<header className="navbar" style={{background: theme.ui , color: theme.syntax ,  }} >
-						<h1>Context App</h1>
-						<div style={Style.menu} >
-							<span>Home</span>
-							<span>About</span>
-							<span>Contact</span>
-						</div>
-					</header>
-				)
-			}}
-			</ThemeContext.Consumer>
+			<AuthContext.Consumer>{(authContext) => (
+					<ThemeContext.Consumer>{(context) => {
+						const {isAuthenticated , toggleAuth} = authContext;
+						const {isLightTheme , light , dark}  = context;
+						const theme = isLightTheme ? light : dark;
+						return(
+							<header className="navbar" style={{background: theme.ui , color: theme.syntax ,  }} >
+								<h1>Context App</h1>
+								<div>
+									{ isAuthenticated ? 'Logged in' : 'Logged Out' }
+								</div>
+								<div style={Style.menu} >
+									<span>Home</span>
+									<span>About</span>
+									<span>Contact</span>
+								</div>
+							</header>
+						)
+					}}
+					</ThemeContext.Consumer>
+				)}
+			</AuthContext.Consumer>
 		);
 	}
 }
