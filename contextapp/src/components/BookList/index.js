74c9af716c58afcb93a903f1aaf0f0c1392947c0
BJ-stack-art <58924072+BJ-stack-art@users.noreply.github.com>
2020-10-26 21:04:25 +0700

    13 - Hooks with Context

--- contextapp/src/components/BookList/index.js
+++ contextapp/src/components/BookList/index.js
@@ -1,24 +1,19 @@
-import React , {Component} from 'react';
+import React , {useContext } from 'react';
 import {ThemeContext} from '../../contexts/ThemeContext.js';
 
-class BookList extends Component {
-	static contextType = ThemeContext;
 
-	render() {
-		const {isLightTheme , light , dark} = this.context;
-		const theme = isLightTheme ? light : dark;
-		return(
-			<React.Fragment>
-				<div className="book-list"  style={{background: theme.bg , color: theme.syntax}}>
-				<ul>
-					<li style={{background: theme.ui}} >the way kings</li>
-					<li style={{background: theme.ui}} >the name of the wind</li>
-					<li style={{background: theme.ui}} >the final empire</li>
-				</ul>
-				</div>
-			</React.Fragment>
-		);
-	}
+const BookList = () => {
+	const {isLightTheme, light , dark} = useContext(ThemeContext);
+	const theme = isLightTheme ? light : dark;
+	return(
+		<div className="book-list"  style={{background: theme.bg , color: theme.syntax}}>
+			<ul>
+				<li style={{background: theme.ui}} >the way kings</li>
+				<li style={{background: theme.ui}} >the name of the wind</li>
+				<li style={{background: theme.ui}} >the final empire</li>
+			</ul>
+		</div>
+	)
 }
 
 export default BookList;
