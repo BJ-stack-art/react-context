a44fe4b9e57eb333eb5b03a533224ba10732807c
BJ-stack-art <58924072+BJ-stack-art@users.noreply.github.com>
2020-10-26 20:54:53 +0700

    12 - useEffect Hooks

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,4 +1,4 @@
-import React , { useState } from 'react';
+import React , { useState , useEffect } from 'react';
 import NewSongForm from './NewSongForm.js';
 
 const SongList = () => {
@@ -7,10 +7,19 @@ const SongList = () => {
 		{ title: 'almost gosple' , id: 2 },
 		{ title: 'almost wild darkness' , id: 3 },
 	]);
+	const [age , setAge] = useState(20);
 
 	const addSong = (title) => {
 		setSongs([...songs , {title , id: Math.random()}]);
 	}
+
+	useEffect(() => {
+		console.log('useeffect hooks ran' , songs);
+	} , [songs])
+
+	useEffect(() => {
+		console.log('useeffect hooks ran' , age);
+	} , [age])
 
 	return(
 		<div className="song-list">
@@ -20,6 +29,7 @@ const SongList = () => {
 				})}
 			</ul>
 			<NewSongForm  addSong={addSong} />
+			<button onClick={() => setAge(age + 1) } >Add 1 to age : {age} </button>
 		</div>
 	)
 }
